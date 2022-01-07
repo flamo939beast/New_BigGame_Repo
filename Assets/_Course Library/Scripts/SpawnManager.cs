c0a76b88a124d91993ba05112397ccc5f8024494
My_Dude <23wingj@live.elida.k12.oh.us>
2022-01-07 09:32:31 -0500

    Added Blank Scripts

--- Assets/_Course Library/Scripts/SpawnManager.cs
+++ Assets/_Course Library/Scripts/SpawnManager.cs
@@ -7,6 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] enemyPrefabs;
     public float spawnRangeX = 5;
     public float spawnPosZ = 20;
+    public float spawnPosY = 0.5f;
     public float startDelay = 2;
     public float spawnInterval = 1.5f;
     // Start is called before the first frame update
@@ -22,7 +23,7 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnEnemy()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, spawnPosZ);
         int enemyIndex = Random.Range(0, enemyPrefabs.Length);
         Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
     }
