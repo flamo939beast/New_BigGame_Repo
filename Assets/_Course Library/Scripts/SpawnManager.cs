84600493636df171b2b19ddae8ebb662df633b22
flamo939beast <23schua@live.elida.k12.oh.us>
2022-01-10 08:04:37 -0500

    DestroyOutOfBounds

--- Assets/_Course Library/Scripts/SpawnManager.cs
+++ Assets/_Course Library/Scripts/SpawnManager.cs
@@ -4,39 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public int enemyCount;
-    public int waveNumber = 1;
+   
     public GameObject[] enemyPrefabs;
     public float spawnRangeX = 5;
     public float spawnPosZ = 20;
     public float spawnPosY = 0.5f;
     public float startDelay = 2;
     public float spawnInterval = 1.5f;
-    public float spawnPosY = .5f;
+   
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(waveNumber);
+        InvokeRepeating("SpawnEnemy", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemyCount = ;
         
-        if(enemyCount == 0)
-        {
-            waveNumber++;
-            SpawnEnemyWave(waveNumber);
-        }
-    }
-
-    void SpawnEnemyWave(int enemiesToSpawn)
-    {
-        for (int i = 0; i < enemiesToSpawn; i++)
-        {
-            InvokeRepeating("SpawnEnemy", startDelay, spawnInterval);
-        }
+        
+      
 
     }
 
