046b35b15cb4919c8eb35ed4fdc62d3b0ace438f
flamo939beast <23schua@live.elida.k12.oh.us>
2022-01-11 09:59:11 -0500

    Waves work and are awsome

--- Assets/_Course Library/Scripts/WaveSpawner.cs
+++ Assets/_Course Library/Scripts/WaveSpawner.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WaveSpawner : MonoBehaviour
 {
+    public TextMeshProUGUI waveCountText;
+    public int waveCount = 1;
+
     public float spawnRate = 1.0f;
     public float timeBetweenWaves = 3.0f;
-
     public int enemyCount;
+   // public GameObject enemy;
 
-    public GameObject enemy;
-    
+    public GameObject[] enemyPrefabs;
+    public float spawnRangeX = 5;
+    public float spawnPosZ = 20;
+    public float spawnPosY = 0.5f;
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
 
 
     bool waveIsDone = true;
@@ -23,6 +31,8 @@ public class WaveSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        waveCountText.text = "Wave: " + waveCount.ToString();
+
         if(waveIsDone == true)
         {
             StartCoroutine(waveSpawner());
@@ -35,14 +45,17 @@ public class WaveSpawner : MonoBehaviour
 
         for(int i = 0; i < enemyCount; i++)
         {
-            GameObject enemyClone = Instantiate(enemy);
-            
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, spawnPosZ);
+            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+            Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+
 
             yield return new WaitForSeconds(spawnRate);
         }
 
         spawnRate -= 0.1f;
-        enemyCount += 3;
+        enemyCount += 1;
+        waveCount += 1;
 
         yield return new WaitForSeconds(timeBetweenWaves);
 
